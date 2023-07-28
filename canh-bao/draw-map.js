7f02fc063497b59a06e72536a2492e8df237793d
nguyenson2003 <59283327+nguyenson2003@users.noreply.github.com>
2023-07-28 09:49:42 +0700

    update

--- canh-bao/draw-map.js
+++ canh-bao/draw-map.js
@@ -1,19 +1,22 @@
 
+var isHasData =[false,false,false,false];
 //default
 var map = L.map('map').fitWorld();
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+var first_lat = 0, first_lng = 0;
 //bật định vị
 function onLocationFound(e) {
     var radius = e.accuracy;
     L.marker(e.latlng).addTo(map)
         .bindPopup("Bạn đang ở đây").openPopup();
     L.circle(e.latlng, radius).addTo(map);
-    var lat = e.latlng.lat;
-    var lng = e.latlng.lng;
-    setWidget(lat, lng, "vị trí của bạn")
+    first_lat = e.latlng.lat;
+    first_lng = e.latlng.lng;
+    waitData();
+    setWidget(first_lat, first_lng, "vị trí của bạn")
 
 }
 map.on('locationfound', onLocationFound);
@@ -28,7 +31,6 @@ function setWidget(lat, lng, nameState) {
     var api_key = 'cd1a5f6ad179bf296cb39abd5e662678';
     if (document.getElementById("weather-body")) {
         var url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${api_key}`;
-        console.log(url);
         fetch(url)
             .then(response => {
                 if (!response.ok) throw new Error(response.statusText);
@@ -63,12 +65,12 @@ function setWidget(lat, lng, nameState) {
                 </div>
                     `;
                 document.getElementById("container-weather").innerHTML = weather_widget;
+                isHasData[0]=true;
             })
             .catch();
     }
     if (document.getElementById("air-widget")) {
         var url2 = `https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lng}&appid=${api_key}`;
-        console.log(url2);
         fetch(url2)
             .then(response => {
                 if (!response.ok) throw new Error(response.statusText);
@@ -88,12 +90,12 @@ function setWidget(lat, lng, nameState) {
                     Mức độ: <span style="color:${getColorLevel(data.list['0'].main.aqi)}">${data.list['0'].main.aqi}</span>/5
                 `;
                 document.getElementById("air-widget").innerHTML = air_widget;
+                isHasData[1]=true;
             })
             .catch()
     }
     if (document.getElementById("rain-widget")) {
         var url3 = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lng}&appid=${api_key}`
-        console.log(url3)
         fetch(url3)
             .then(response => {
                 if (!response.ok) throw new Error(response.statusText);
@@ -125,6 +127,7 @@ function setWidget(lat, lng, nameState) {
                 <i>(giá trị được ước tính chung trên lý thuyết, ở một số nơi như thành thị sẽ có khả năng xảy ra lũ thấp hơn dự kiến)</i>
                 `
                 document.getElementById("rain-widget").innerHTML = rain_widget;
+                isHasData[2]=true;
 
 
             })
@@ -132,7 +135,6 @@ function setWidget(lat, lng, nameState) {
     }
     if (document.getElementById("wind-widget")) {
         var url3 = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lng}&appid=${api_key}`
-        console.log(url3)
         fetch(url3)
             .then(response => {
                 if (!response.ok) throw new Error(response.statusText);
@@ -143,9 +145,9 @@ function setWidget(lat, lng, nameState) {
                 var max_wind = 0;
                 for (let i = 0; i < 5; i++) {
                     for (let j = 0; j < 6; j++) {
-                        list_wind[i] =Math.max(list_wind[i], data.list[i * 6 + j].wind.speed);
+                        list_wind[i] = Math.max(list_wind[i], data.list[i * 6 + j].wind.speed);
                     }
-                    list_wind[i]*=3.6;
+                    list_wind[i] *= 3.6;
                     max_wind = Math.max(list_wind[i], max_wind);
                 }
                 var wind_widget = `
@@ -157,27 +159,122 @@ function setWidget(lat, lng, nameState) {
                     <li>Ngày 4: ${list_wind[3].toFixed(2)}km/h</li>
                     <li>Ngày 5: ${list_wind[4].toFixed(2)}km/h</li>
                 </ul>
-                Dựa vào bảng số liệu có thể thấy 5 ngày tiếp theo: <span style="color:#B4B1FF;font-weight:bolder">${
-                    max_wind<19?'Gió nhẹ, không gây nguy hại.':
-                    max_wind<38?'Cây nhỏ có lá bắt đầu lay động, ảnh hưởng đến lúa đang phơi màu. Biển hơi động. Thuyền đánh cá bị chao nghiêng, phải cuốn bớt buồm.':
-                    max_wind<61?'Cây cối rung chuyển. Khó đi ngược gió. Biển động. Nguy hiểm đối với tàu, thuyền.':
-                    max_wind<88?'Gió làm gãy cành cây, tốc mái nhà gây thiệt hại về nhà cửa. Không thể đi ngược gió. Biển động rất mạnh. Rất nguy hiểm đối với tàu, thuyền.':
-                    max_wind<117?'Làm đổ cây cối, nhà cửa, cột điện. Gây thiệt hại rất nặng. Biển động dữ đội. Làm đắm tàu biển.':
-                    'Sức phá hoại cực kỳ lớn. Sóng biển cực kỳ mạnh. Đánh đắm tàu biển có trọng tải lớn.'
-                    
-                }</span>
+                Dựa vào bảng số liệu có thể thấy 5 ngày tiếp theo: <span style="color:#B4B1FF;font-weight:bolder">${max_wind < 19 ? 'Gió nhẹ, không gây nguy hại.' :
+                        max_wind < 38 ? 'Cây nhỏ có lá bắt đầu lay động, ảnh hưởng đến lúa đang phơi màu. Biển hơi động. Thuyền đánh cá bị chao nghiêng, phải cuốn bớt buồm.' :
+                            max_wind < 61 ? 'Cây cối rung chuyển. Khó đi ngược gió. Biển động. Nguy hiểm đối với tàu, thuyền.' :
+                                max_wind < 88 ? 'Gió làm gãy cành cây, tốc mái nhà gây thiệt hại về nhà cửa. Không thể đi ngược gió. Biển động rất mạnh. Rất nguy hiểm đối với tàu, thuyền.' :
+                                    max_wind < 117 ? 'Làm đổ cây cối, nhà cửa, cột điện. Gây thiệt hại rất nặng. Biển động dữ đội. Làm đắm tàu biển.' :
+                                        'Sức phá hoại cực kỳ lớn. Sóng biển cực kỳ mạnh. Đánh đắm tàu biển có trọng tải lớn.'
+
+                    }</span>
                 `
                 document.getElementById("wind-widget").innerHTML = wind_widget;
+                isHasData[3]=true;
 
 
             })
             .catch();
     }
 }
+function waitData(nameState) {
+    if(nameState==null)nameState="vị trí của bạn"
+    if (document.getElementById("weather-body") && !isHasData[0]) {
+        var weather_widget = `
+                <div class="weather-header" style="text-transform: capitalize;">
+                    ${nameState}
+                </div>
+                <div id="weather-body">
+                    <div class="weather-right__temperature">${(Math.random() * 30).toFixed(0)}<span>°C</span></div>
+                    <table class="weather-right__table">
+                        <tbody>
+                            <tr class="weather-right__items">
+                                <th colspan="2" class="weather-right__item">Chi tiết</th>
+                            </tr>
+                            <tr class="weather-right__items">
+                                <td class="weather-right__item">Nhiệt độ cảm nhận</td>
+                                <td class="weather-right__item weather-right__feels">${(Math.random() * 30).toFixed(1)}<span>°C</span></td>
+                            </tr>
+                            <tr class="weather-right__items">
+                                <td class="weather-right__item">Tốc độ gió</td>
+                                <td class="weather-right__item weather-right__wind-speed">${(Math.random() * 30).toFixed(1)} m/s </td>
+                            </tr>
+                            <tr class="weather-right-card__items">
+                                <td class="weather-right__item">Độ ẩm</td>
+                                <td class="weather-right__item weather-right__humidity">${(Math.random() * 30).toFixed(1)}%</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                    `;
+        document.getElementById("container-weather").innerHTML = weather_widget;
+    }
+    if (document.getElementById("air-widget")&& !isHasData[1]) {
+        var air_widget = `
+            Nồng độ không khí ô nhiễm ở ${nameState}:
+            <ul>
+                <li>SO<sub>2</sub>: ${(Math.random() * 30).toFixed(1)} μg/m<sup>3</sup></li>
+                <li>NO<sub>2</sub>: ${(Math.random() * 30).toFixed(1)} μg/m<sup>3</sup></li>
+                <li>PM<sub>10</sub>: ${(Math.random() * 30).toFixed(1)} μg/m<sup>3</sup></li>
+                <li>PM<sub>2.5</sub>: ${(Math.random() * 30).toFixed(1)} μg/m<sup>3</sup></li>
+                <li>O<sub>3</sub>: ${(Math.random() * 30).toFixed(1)} μg/m<sup>3</sup></li>
+                <li>CO: ${(Math.random() * 30).toFixed(1)} μg/m<sup>3</sup></li>
+            </ul>
+            Mức độ: ${(Math.random() * 10).toFixed(0)}/5
+        `;
+        document.getElementById("air-widget").innerHTML = air_widget;
+    }
+    if (document.getElementById("rain-widget")&& !isHasData[2]) {
+        var rain_widget = `
+        Đánh giá lượng mưa ở ${nameState}:
+        <ul>
+            <li>Tổng lượng mưa trong 5 ngày: ${(Math.random() * 30).toFixed(1)}mm</li>
+            <ul>
+                <li>Hôm nay: ${(Math.random() * 30).toFixed(1)}mm</li>
+                <li>Ngày 2: ${(Math.random() * 30).toFixed(1)}mm</li>
+                <li>Ngày 3: ${(Math.random() * 30).toFixed(1)}mm</li>
+                <li>Ngày 4: ${(Math.random() * 30).toFixed(1)}mm</li>
+                <li>Ngày 5: ${(Math.random() * 30).toFixed(1)}mm</li>
+            </ul>
+        </ul>
+        Khả năng xãy ra mưa lũ: <span style="color:#B4B1FF;font-weight:bolder">${(Math.random() * 30).toFixed(1)}%</span> <br>
+        <i>(giá trị được ước tính chung trên lý thuyết, ở một số nơi như thành thị sẽ có khả năng xảy ra lũ thấp hơn dự kiến)</i>
+        `
+        document.getElementById("rain-widget").innerHTML = rain_widget;
+    }
+    if (document.getElementById("wind-widget")&& !isHasData[3]) {
+        var max_wind=(Math.random() * 30).toFixed(1);
+        var wind_widget = `
+                Tốc độ gió tối đa trong 5 ngày tới tại ${nameState}:
+                <ul>
+                    <li>Hôm nay: ${(Math.random() * 30).toFixed(1)}km/h</li>
+                    <li>Ngày 2: ${(Math.random() * 30).toFixed(1)}km/h</li>
+                    <li>Ngày 3: ${(Math.random() * 30).toFixed(1)}km/h</li>
+                    <li>Ngày 4: ${(Math.random() * 30).toFixed(1)}km/h</li>
+                    <li>Ngày 5: ${(Math.random() * 30).toFixed(1)}km/h</li>
+                </ul>
+                Dựa vào bảng số liệu có thể thấy 5 ngày tiếp theo: <span style="color:#B4B1FF;font-weight:bolder">${max_wind < 19 ? 'Gió nhẹ, không gây nguy hại.' :
+                max_wind < 38 ? 'Cây nhỏ có lá bắt đầu lay động, ảnh hưởng đến lúa đang phơi màu. Biển hơi động. Thuyền đánh cá bị chao nghiêng, phải cuốn bớt buồm.' :
+                    max_wind < 61 ? 'Cây cối rung chuyển. Khó đi ngược gió. Biển động. Nguy hiểm đối với tàu, thuyền.' :
+                        max_wind < 88 ? 'Gió làm gãy cành cây, tốc mái nhà gây thiệt hại về nhà cửa. Không thể đi ngược gió. Biển động rất mạnh. Rất nguy hiểm đối với tàu, thuyền.' :
+                            max_wind < 117 ? 'Làm đổ cây cối, nhà cửa, cột điện. Gây thiệt hại rất nặng. Biển động dữ đội. Làm đắm tàu biển.' :
+                                'Sức phá hoại cực kỳ lớn. Sóng biển cực kỳ mạnh. Đánh đắm tàu biển có trọng tải lớn.'
+
+            }</span>
+                `
+        document.getElementById("wind-widget").innerHTML = wind_widget;
+    }
+    if(!isHasData[0] || !isHasData[1] || !isHasData[2] || !isHasData[3])
+        setTimeout(waitData, 10);
+}
 //get position in map
 var popup = L.popup();
 function onMapClick(e) {
-    setWidget(e.latlng.lat, e.latlng.lng, curState);
+    isHasData=[false,false,false,false]
+    waitData(curState);
+    if (curState == "vị trí của bạn")
+        setWidget(first_lat, first_lng, curState);
+    else
+        setWidget(e.latlng.lat, e.latlng.lng, curState);
     document.getElementById('main-body').scrollIntoView();
 }
 map.on('click', onMapClick);
@@ -233,7 +330,7 @@ info.onAdd = function (map) {
     this.update();
     return this._div;
 };
-var curState = '';
+var curState = 'vị trí của bạn';
 // method that we will use to update the control based on feature properties passed
 info.update = function (props) {
     this._div.innerHTML = '<h4>Vietnam counntry name</h4>' + (props ?
